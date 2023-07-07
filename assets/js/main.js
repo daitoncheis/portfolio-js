a044f256ece2127683bc81fc1799b0131540cbcb
daitoncheis <daitoncheis@gmail.com>
2023-07-07 01:46:02 -0300

    portfolio update json

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,18 +24,41 @@ function updateProfileInfo(profileData) {
 function updateSoftSkills(profileData) {
   const softSkills = document.getElementById("profile.skills.softSkills");
 
-  softSkills.innerHTML = profileData.skills.softSkills.map((skills) => `<li>${skills}</li>`).join("");
+  softSkills.innerHTML = profileData.skills.softSkills
+    .map((skills) => `<li>${skills}</li>`)
+    .join("");
 }
 
 function updateHardSkills(profileData) {
-    const hardSkills = document.getElementById("profile.skills.hardSkills");
-  
-    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"</li>`).join("")
-  }
+  const hardSkills = document.getElementById("profile.skills.hardSkills");
+
+  hardSkills.innerHTML = profileData.skills.hardSkills
+    .map(
+      (skill) =>
+        `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"</li>`
+    )
+    .join("");
+}
 
 function updateLanguages(profileData) {
-  const languages = document.getElementById('profile.languages')
-  languages.innerHTML = profileData.languages.map(languages => `<li>${languages}</li>`).join('')
+  const languages = document.getElementById("profile.languages");
+  languages.innerHTML = profileData.languages
+    .map((languages) => `<li>${languages}</li>`)
+    .join("");
+}
+
+function updatePortfolio(profileData) {
+  const portfolio = document.getElementById("profile.portfolio");
+  portifolio.innerHTML = profileData.portifolio
+    .map((project) => {
+      return ` 
+      <li>
+        <h3 ${project.github ? 'class="github"' : ""}>${project.name}</h3>
+        <a href="${project.url}" target="_blank">${project.url}</a>
+      </li>
+    `;
+    })
+    .join("");
 }
 
 (async () => {
@@ -43,5 +66,6 @@ function updateLanguages(profileData) {
   updateProfileInfo(profileData);
   updateSoftSkills(profileData);
   updateHardSkills(profileData);
-  updateLanguages(profileData)
+  updateLanguages(profileData);
+  updatePortfolio(profileData);
 })();
